08723ed68fd235dbcc1f9151ba581225e3886808
Enio Fernandes <ef@cic.ac>
2017-05-20 13:46:23 +0000

    first

---
+++ kerasInception.py
@@ -0,0 +1,13 @@
+from tensorflow.contrib.keras.python.keras.preprocessing import image
+from tensorflow.contrib.keras.python.keras.applications.inception_v3 import *
+import numpy as np
+
+model = InceptionV3(weights='imagenet')
+
+img = image.load_img('image.jpg', target_size= (299, 299))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+preds = model.predict(x)
+print('Predicted:', decode_predictions(preds, top=5)[0])
