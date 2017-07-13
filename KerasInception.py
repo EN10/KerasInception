1c900f924cda6ef2a465aab59a9490415ce7957c
Enio Fernandes <ef@cic.ac>
2017-07-13 16:44:19 +0000

    predict new line

--- KerasInception.py
+++ KerasInception.py
@@ -10,4 +10,5 @@ x = np.expand_dims(x, axis=0)
 x = preprocess_input(x)
 
 preds = model.predict(x)
-print('Predicted:', decode_predictions(preds, top=5)[0])
+for i in range(5):
+    print('Predicted:', decode_predictions(preds, top=5)[0][i])
