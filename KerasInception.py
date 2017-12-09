c0bc9cbd6146aaf091305d031a3fbc7325d1cbba
Enio Fernandes <ef@cic.ac>
2017-12-09 10:50:40 +0000

    update keras path

--- KerasInception.py
+++ KerasInception.py
@@ -1,5 +1,5 @@
-from tensorflow.contrib.keras.python.keras.preprocessing import image
-from tensorflow.contrib.keras.python.keras.applications.inception_v3 import *
+from tensorflow.python.keras.preprocessing import image
+from tensorflow.python.keras.applications.inception_v3 import *
 import numpy as np
 
 model = InceptionV3(weights='imagenet')
@@ -9,6 +9,6 @@ x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
 x = preprocess_input(x)
 
-preds = model.predict(x)
-for i in range(5):
-    print('Predicted:', decode_predictions(preds, top=5)[0][i])
+y = model.predict(x)
+for index, res in enumerate(decode_predictions(y)[0]):
+    print('{}. {}: {:.3f}%'.format(index + 1, res[1], 100 * res[2]))
