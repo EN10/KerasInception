8ff2acf17e65c997349a03def87bc05101096fd1
Enio Fernandes <ef@cic.ac>
2018-01-11 16:26:14 +0000

    keras

--- models/run.sh
+++ models/run.sh
@@ -1,9 +1,16 @@
-# floyd run --data efcic/datasets/nasnet-large/1:models --env tensorflow-1.4 "bash run.sh"
+# floyd run --data efcic/datasets/nasnet-large/1:models --data efcic/datasets/keras/1:keras --env tensorflow-1.4 "bash run.sh"
 
+# Setup NASnet Model
 mkdir -p /root/.keras/models
-
 cp /models/nasnet_large.h5 /root/.keras/models/nasnet_large.h5
 
-pip install git+git://github.com/keras-team/keras.git --upgrade --no-deps
+# Install Keras 2.1.2
+mkdir -p /keras-git
+cp -a /keras/. /keras-git/
+cd /keras-git
+sudo pip install -e . --upgrade --no-deps
+pip show keras
 
+# Run NASnet
+cd /code
 python nasnet.py
